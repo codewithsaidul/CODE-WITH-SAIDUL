fbf5d5f945b8f4e7a45a52a2bf9bd0bce059b46e
saidul83 <saidulislamr333@gmail.com>
2022-08-20 23:56:41 +0600

    w3c

--- js/main.js
+++ js/main.js
@@ -303,5 +303,5 @@ window.addEventListener("load", () =>{
     document.querySelector(".preloader").classList.add("fade-out");
     setTimeout(() =>{
         document.querySelector(".preloader").style.display = "none";  
-    }, 4000)
+    }, 2000)
 })
